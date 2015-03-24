e79447a71973d5761db6caac5e38b5df07d6a1e1
mike vallano <mikevallano@gmail.com>
2015-03-23 20:04:29 -0500

    updated form and nav.

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -69,6 +69,6 @@ class ResourcesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def resource_params
-      params.require(:resource).permit(:name, :tldr, :link)
+      params.require(:resource).permit(:name, :tldr, :link, :category_ids => [])
     end
 end

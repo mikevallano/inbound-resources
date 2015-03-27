21f1b6584cdb3c0fb363f9b23489299ae890b0e8
mike vallano <mikevallano@gmail.com>
2015-03-27 14:04:47 -0500

    more config for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
     port: 587,
-    domain: Rails.application.secrets.domain_name,
+    domain: 'heroku.com',
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV['SENDGRID_USERNAME'],
